153068413bb045109aa45707b42dea0aef277738
zeshan <zeshanm.zaki@gmail.com>
2025-02-24 16:31:52 -0500

    remove unneeded code in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,7 @@ const App = () => {
   return (
     <div style={{ transform: "scale(1.1)", transformOrigin: "top left", width: "90vw", height: "90vh" }}>
       <HeroBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Home />
     </div>
   )
 }
