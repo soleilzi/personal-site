6a824378b04d4f5515b0e698ba0abaa3ec333552
zeshan <zeshanm.zaki@gmail.com>
2025-04-25 18:31:45 -0400

    visual edits

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import info from "../info.json"
-import bookpostpic from "../assets/bookpost.jpg"
-import sajamslam2pic from "../assets/sajamslam2.png"
+import bookpostpic from "../assets/bookpostimg.jpg"
+import sajamslam2pic from "../assets/sajamslamimg.png"
+import movemovieimg from "../assets/movemovieimg.jpg"
 import { motion } from "motion/react"
 
 const Home = () => {
   return (
-    <div className='p-4 mt-5 max-w-xl min-h-72 mx-auto border-x-[0.1px] border-gray-800 font-mono'>
+    <div className='py-4 px-8 sm:p-4 mt-5 max-w-xl min-h-72 mx-auto border-x-[0.1px] border-gray-800 font-mono'>
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
@@ -107,9 +108,9 @@ const Home = () => {
       >
         <div className='p-4 bg-slate-800 mt-10'>
           <h2 className='text-emerald-400'>My Work</h2>
-          <div className='p-2 grid grid-cols-2 gap-x-4'>
+          <div className='p-2 grid grid-cols-1 sm:grid-cols-2 justify-center gap-4'>
             <a href={info.links.bookpost} target="_blank" rel="noopener noreferrer">
-              <div className='transition duration-300 hover:-translate-y-2 hover:shadow-xl'>
+              <div className='w-full transition duration-300 hover:-translate-y-2 hover:shadow-xl'>
                 <h3>BookPost</h3>
                 <img src={bookpostpic} className='h-[155px]' />
               </div>
@@ -119,6 +120,13 @@ const Home = () => {
               <div className='transition duration-300 hover:-translate-y-2 hover:shadow-xl'>
                 <h3>Sajam Slam 2</h3>
                 <img src={sajamslam2pic} className='h-[155px]' />
+              </div>
+            </a>
+
+            <a href={info.links.movemovie} target="_blank" rel="noopener noreferrer">
+              <div className='transition duration-300 hover:-translate-y-2 hover:shadow-xl'>
+                <h3>MoveMovie</h3>
+                <img src={movemovieimg} className='h-[155px]' />
               </div>
             </a>
           </div>
